7e35062b7ee38df454495679e90adc441a50558b
rcom10002 <rcom10002@e0633416-5d6b-11de-8d7d-01d876f1962d>
2009-07-30 15:41:15 +0000

    积分统计

---
+++ f3server/javaweb/info/knightrcom/web/model/entity/ReportScoreInfo.java
@@ -0,0 +1,152 @@
+package info.knightrcom.web.model.entity;
+
+import java.math.BigInteger;
+
+public class ReportScoreInfo {
+	private String userId;
+	
+	private BigInteger totalTimes;
+	
+	private String totalScores;
+	
+	private BigInteger winTimes;
+	
+	private String winScores;
+	
+	private BigInteger loseTimes;
+	
+	private String loseScores;
+	
+	private BigInteger drawTimes;
+	
+	private String drawScores;
+
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the totalTimes
+	 */
+	public BigInteger getTotalTimes() {
+		return totalTimes;
+	}
+
+	/**
+	 * @param totalTimes the totalTimes to set
+	 */
+	public void setTotalTimes(BigInteger totalTimes) {
+		this.totalTimes = totalTimes;
+	}
+
+	/**
+	 * @return the totalScores
+	 */
+	public String getTotalScores() {
+		return totalScores;
+	}
+
+	/**
+	 * @param totalScores the totalScores to set
+	 */
+	public void setTotalScores(String totalScores) {
+		this.totalScores = totalScores;
+	}
+
+	/**
+	 * @return the winTimes
+	 */
+	public BigInteger getWinTimes() {
+		return winTimes;
+	}
+
+	/**
+	 * @param winTimes the winTimes to set
+	 */
+	public void setWinTimes(BigInteger winTimes) {
+		this.winTimes = winTimes;
+	}
+
+	/**
+	 * @return the winScores
+	 */
+	public String getWinScores() {
+		return winScores;
+	}
+
+	/**
+	 * @param winScores the winScores to set
+	 */
+	public void setWinScores(String winScores) {
+		this.winScores = winScores;
+	}
+
+	/**
+	 * @return the loseTimes
+	 */
+	public BigInteger getLoseTimes() {
+		return loseTimes;
+	}
+
+	/**
+	 * @param loseTimes the loseTimes to set
+	 */
+	public void setLoseTimes(BigInteger loseTimes) {
+		this.loseTimes = loseTimes;
+	}
+
+	/**
+	 * @return the loseScores
+	 */
+	public String getLoseScores() {
+		return loseScores;
+	}
+
+	/**
+	 * @param loseScores the loseScores to set
+	 */
+	public void setLoseScores(String loseScores) {
+		this.loseScores = loseScores;
+	}
+
+	/**
+	 * @return the drawTimes
+	 */
+	public BigInteger getDrawTimes() {
+		return drawTimes;
+	}
+
+	/**
+	 * @param drawTimes the drawTimes to set
+	 */
+	public void setDrawTimes(BigInteger drawTimes) {
+		this.drawTimes = drawTimes;
+	}
+
+	/**
+	 * @return the drawScores
+	 */
+	public String getDrawScores() {
+		return drawScores;
+	}
+
+	/**
+	 * @param drawScores the drawScores to set
+	 */
+	public void setDrawScores(String drawScores) {
+		this.drawScores = drawScores;
+	}
+
+	
+	
+}
--- f3server/javaweb/info/knightrcom/web/service/ReportScoreService.java
+++ f3server/javaweb/info/knightrcom/web/service/ReportScoreService.java
@@ -1,11 +1,16 @@
 package info.knightrcom.web.service;
 
+import info.knightrcom.data.HibernateSessionFactory;
+import info.knightrcom.data.metadata.PeriodlySum;
 import info.knightrcom.data.metadata.PlayerScore;
-import info.knightrcom.util.StringHelper;
-import info.knightrcom.web.model.entity.TodayInfo;
+import info.knightrcom.web.model.entity.ReportScoreInfo;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.hibernate.Query;
 import org.hibernate.transform.ResultTransformer;
 import org.hibernate.transform.Transformers;
@@ -13,12 +18,12 @@ import org.hibernate.transform.Transformers;
 public class ReportScoreService extends F3SWebService<PlayerScore> {
 
 	public void processQuerySetting(Query query, HttpServletRequest request) {
-        query.setString(0, request.getParameter("CURRENT_PROFILE_ID"));
-        query.setString(1, request.getParameter("CURRENT_PROFILE_ID"));
-        query.setTimestamp(2, StringHelper.toTimeStamp(request.getParameter("FROM_DATE"), "yyyyMMdd"));
-        query.setTimestamp(3, StringHelper.toTimeStamp(request.getParameter("TO_DATE"), "yyyyMMdd"));
-        query.setInteger(4, new Integer(request.getParameter("SHOW_CONDITION")));
-        query.setInteger(5, new Integer(request.getParameter("SHOW_CONDITION")));
+//        query.setString(0, request.getParameter("CURRENT_PROFILE_ID"));
+//        query.setString(1, request.getParameter("CURRENT_PROFILE_ID"));
+//        query.setTimestamp(2, StringHelper.toTimeStamp(request.getParameter("FROM_DATE"), "yyyyMMdd"));
+//        query.setTimestamp(3, StringHelper.toTimeStamp(request.getParameter("TO_DATE"), "yyyyMMdd"));
+//        query.setInteger(4, new Integer(request.getParameter("SHOW_CONDITION")));
+//        query.setInteger(5, new Integer(request.getParameter("SHOW_CONDITION")));
     }
 	
     @Override
@@ -33,12 +38,29 @@ public class ReportScoreService extends F3SWebService<PlayerScore> {
 
     @Override
     public ResultTransformer getResultTransformer() {
-    	return Transformers.aliasToBean(TodayInfo.class);
+    	return Transformers.aliasToBean(ReportScoreInfo.class);
     }
 
 
     @Override
     public Class<?>[] getAliasTypes() {
-    	return new Class<?>[] {PlayerScore.class, TodayInfo.class};
+    	return new Class<?>[] {PlayerScore.class, ReportScoreInfo.class};
+    }
+    
+    /**
+     * 将统计出的结果手插入到periodly_sum表
+     * 防止重复查询
+     */
+    public void insertPeriodlySum() {
+    	List<ReportScoreInfo> result = HibernateSessionFactory.getSession().getNamedQuery(getNamedQuery()).list();
+    	for (ReportScoreInfo rs : result) {
+    		PeriodlySum periodSum = new PeriodlySum();
+    		try {
+				PropertyUtils.copyProperties(periodSum, rs);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    		HibernateSessionFactory.getSession().save(periodSum);
+    	}
     }
 }
