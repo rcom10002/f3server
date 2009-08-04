2eac7f9868d613ab6732083bea8a0118bbddb774
rcom10002 <rcom10002@e0633416-5d6b-11de-8d7d-01d876f1962d>
2009-08-04 13:11:12 +0000

    下级用户创建的下下级则为

--- f3server/javaweb/info/knightrcom/web/service/ReportBusinessService.java
+++ f3server/javaweb/info/knightrcom/web/service/ReportBusinessService.java
@@ -16,13 +16,14 @@ public class ReportBusinessService extends F3SWebService<PlayerScore> {
 
         query.setTimestamp(0, StringHelper.toTimeStamp(request.getParameter("FROM_DATE"), "yyyyMMdd"));
         query.setTimestamp(1, StringHelper.toTimeStamp(request.getParameter("TO_DATE"), "yyyyMMdd"));
-        query.setString(2, request.getParameter("USER_ID"));
-        query.setString(3, request.getParameter("USER_ID"));
-        query.setString(4, request.getParameter("USER_ID"));
-        query.setTimestamp(5, StringHelper.toTimeStamp(request.getParameter("FROM_DATE"), "yyyyMMdd"));
-        query.setTimestamp(6, StringHelper.toTimeStamp(request.getParameter("TO_DATE"), "yyyyMMdd"));
-        query.setInteger(7, new Integer(request.getParameter("SHOW_CONDITION")));
-        query.setInteger(8, new Integer(request.getParameter("SHOW_CONDITION")));
+        String userId = StringHelper.escapeSQL(request.getParameter("USER_ID")) == null ? "" : StringHelper.escapeSQL(request.getParameter("USER_ID"));
+        query.setString(2, "%" + userId + "%");
+        query.setTimestamp(3, StringHelper.toTimeStamp(request.getParameter("FROM_DATE"), "yyyyMMdd"));
+        query.setTimestamp(4, StringHelper.toTimeStamp(request.getParameter("TO_DATE"), "yyyyMMdd"));
+        query.setInteger(5, new Integer(request.getParameter("SHOW_CONDITION")));
+        query.setInteger(6, new Integer(request.getParameter("SHOW_CONDITION")));
+        query.setString(7, StringHelper.escapeSQL(request.getParameter("USER_ID")));
+        query.setString(8, userId + "%");
     }
 
     public String getNamedQuery() {
