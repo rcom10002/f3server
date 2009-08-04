90de1a519ea5607e687928024d4bdf36d449f909
rcom10002 <rcom10002@e0633416-5d6b-11de-8d7d-01d876f1962d>
2009-08-04 13:11:12 +0000

    下级用户创建的下下级则为

--- trunk/f3server/javaweb/info/knightrcom/web/service/ReportScoreService.java
+++ trunk/f3server/javaweb/info/knightrcom/web/service/ReportScoreService.java
@@ -26,6 +26,8 @@ public class ReportScoreService extends F3SWebService<PeriodlySum> {
         query.setTimestamp(1, StringHelper.toTimeStamp(request.getParameter("TO_DATE"), "yyyyMMdd"));
         String userId = StringHelper.escapeSQL(request.getParameter("USER_ID")) == null ? "" : StringHelper.escapeSQL(request.getParameter("USER_ID"));
         query.setString(2, "%" + userId + "%");
+        query.setString(3, StringHelper.escapeSQL(request.getParameter("USER_ID")));
+        query.setString(4, userId + "%");
 	}
 	
 	/**
@@ -49,6 +51,8 @@ public class ReportScoreService extends F3SWebService<PeriodlySum> {
 		
         String userId = StringHelper.escapeSQL(request.getParameter("USER_ID")) == null ? "" : StringHelper.escapeSQL(request.getParameter("USER_ID"));
         query.setString(12, "%" + userId + "%");
+        query.setString(13, StringHelper.escapeSQL(request.getParameter("USER_ID")));
+        query.setString(14, userId + "%");
 	}
 	
     @Override
