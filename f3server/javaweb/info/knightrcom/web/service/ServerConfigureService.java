0f8ac582d51a623aa4f9b524c363d068defcd4b8
rcom10002 <rcom10002@e0633416-5d6b-11de-8d7d-01d876f1962d>
2009-08-12 07:42:37 +0000

    server param update

--- f3server/javaweb/info/knightrcom/web/service/ServerConfigureService.java
+++ f3server/javaweb/info/knightrcom/web/service/ServerConfigureService.java
@@ -2,44 +2,36 @@ package info.knightrcom.web.service;
 
 import info.knightrcom.data.HibernateSessionFactory;
 import info.knightrcom.data.metadata.GlobalConfig;
-import info.knightrcom.util.ModelUtil;
+import info.knightrcom.data.metadata.GlobalConfigDAO;
 import info.knightrcom.web.constant.GameConfigureConstant;
 import info.knightrcom.web.model.EntityInfo;
 
-import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Map.Entry;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.Criteria;
 import org.hibernate.Query;
-import org.hibernate.criterion.Expression;
 import org.hibernate.transform.ResultTransformer;
 import org.hibernate.transform.Transformers;
 
 @SuppressWarnings("unchecked")
-public class ServerConfigureService extends F3SWebService<List<Map>> {
+public class ServerConfigureService extends F3SWebService<GlobalConfig> {
 
     @Override
     public Class<?>[] getAliasTypes() {
-    	return new Class<?>[] {List.class};
+    	return new Class<?>[] {GlobalConfig.class};
     }
 
     @Override
     public String getNamedQuery() {
-    	 return "GLOBAL_CONFIG";
+    	 return "GLOBAL_CONFIGURE";
     }
 
     @Override
     public String getNamedQueryForCount() {
-    	return "GLOBAL_CONFIG_COUNT";
+    	return "GLOBAL_CONFIGURE_COUNT";
     }
 
     @Override
@@ -49,7 +41,7 @@ public class ServerConfigureService extends F3SWebService<List<Map>> {
 
     @Override
     public void processQuerySetting(Query query, HttpServletRequest request) {
-        
+    	query.setString(0, GameConfigureConstant.SERVER_PARAM_NAME);
     }
     
     /**
@@ -59,27 +51,11 @@ public class ServerConfigureService extends F3SWebService<List<Map>> {
 	 * @return
 	 */
 	public String READ_SERVER_CONFIGURE(HttpServletRequest request, HttpServletResponse response) {
-    	// 设置页码
-        int currentPage = 1;
-        if (request.getParameter("CURRENT_PAGE").matches("[1-9]\\d*")) {
-            currentPage = new Integer(request.getParameter("CURRENT_PAGE")).intValue();
-        }
-    	Properties configParams = ModelUtil.readProperties(GameConfigureConstant.SERVER_PARAM_NAME);
-    	
-    	List<Map> configMaps = new ArrayList<Map>();
-    	for (Entry<Object, Object> param : configParams.entrySet()) {
-    		Map map = new HashMap();
-    		map.put(param.getKey(), param.getValue());
-    		configMaps.add(map);
-	    }
-
-        EntityInfo<List<Map>> info = new EntityInfo<List<Map>>();
+		final GlobalConfig globalconfig = new GlobalConfigDAO().findById(request.getParameter("GLOBALCONFIG_ID"));
+        EntityInfo<GlobalConfig> info = new EntityInfo<GlobalConfig>();
+        info.setEntity(globalconfig);
         info.setResult(F3SWebServiceResult.SUCCESS);
-        info.setEntity(configMaps);
-        info.getPagination().setPageSize(configMaps.size());
-        info.getPagination().setTotalRecord(configMaps.size()*configMaps.size());
-        info.getPagination().setCurrentPage(currentPage);
-        return toXML(info, getAliasTypes());
+        return toXML(info, new Class[] {GlobalConfig.class});
     }
 	
 	/**
@@ -89,29 +65,20 @@ public class ServerConfigureService extends F3SWebService<List<Map>> {
 	 * @return
 	 */
 	public String CREATE_SERVER_CONFIGURE(HttpServletRequest request, HttpServletResponse response) {
-		EntityInfo<List<Map>> info = new EntityInfo<List<Map>>();
-    	try {
-    		String variableName = request.getParameter("VARIABLE_NAME");
-	        String variableValue = request.getParameter("VARIABLE_VALUE");
-	        Properties propertiesOrig = ModelUtil.readProperties(GameConfigureConstant.SERVER_PARAM_NAME);
-	        propertiesOrig.setProperty(variableName.toUpperCase(), variableValue);
-	        // 数据更新至DB
-	    	// 获取原始记录
-	    	Criteria criteria = HibernateSessionFactory.getSession().createCriteria(GlobalConfig.class);
-	    	criteria.add(Expression.eq("name", GameConfigureConstant.SERVER_PARAM_NAME));
-	    	GlobalConfig globalConfig = (GlobalConfig) criteria.uniqueResult();
-			// 更新记录
-			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-			propertiesOrig.storeToXML(outStream, null);
-			globalConfig.setValue(outStream.toString("utf-8"));
-			globalConfig.setUpdateTime(new Date());
-			HibernateSessionFactory.getSession().save(globalConfig);
-			info.setResult(F3SWebServiceResult.SUCCESS);
-        } catch (Exception e) {
-        	e.printStackTrace();
-        	info.setResult(F3SWebServiceResult.FAIL);
-        }
-        return toXML(info, getAliasTypes());
+		String name = request.getParameter("VARIABLE_NAME");
+        String value = request.getParameter("VARIABLE_VALUE");
+		GlobalConfig globalconfig = new GlobalConfig();
+		globalconfig.setGlobalConfigId(UUID.randomUUID().toString());
+		globalconfig.setName(name);
+		globalconfig.setValue(value);
+		globalconfig.setType(GameConfigureConstant.SERVER_PARAM_NAME);
+		globalconfig.setCreateTime(new Date());
+		globalconfig.setUpdateTime(new Date());
+        HibernateSessionFactory.getSession().save(globalconfig);
+        EntityInfo<GlobalConfig> info = new EntityInfo<GlobalConfig>();
+        info.setEntity(globalconfig);
+        info.setResult(F3SWebServiceResult.SUCCESS);
+        return toXML(info, new Class[] {GlobalConfig.class});
     }
     
 	/**
@@ -121,37 +88,16 @@ public class ServerConfigureService extends F3SWebService<List<Map>> {
 	 * @return
 	 */
 	public String UPDATE_SERVER_CONFIGURE(HttpServletRequest request, HttpServletResponse response) {
-		EntityInfo<List<Map>> info = new EntityInfo<List<Map>>();
-    	try {
-	        String variableName = request.getParameter("VARIABLE_NAME");
-	        String variableNameTager = toStandVariableName(variableName);
-	        String variableValue = request.getParameter("VARIABLE_VALUE");
-	        Properties properties = ModelUtil.readProperties(GameConfigureConstant.SERVER_PARAM_NAME);
-	    	
-	    	for (Entry<Object, Object> param : properties.entrySet()) {
-	    		// 更新变量值
-	    		if (param.getKey().equals(variableNameTager)) {
-	    			 properties.setProperty(variableNameTager, variableValue);
-	    		}
-		    }
-	        
-	        // 数据更新至DB
-	    	// 获取原始记录
-	    	Criteria criteria = HibernateSessionFactory.getSession().createCriteria(GlobalConfig.class);
-	    	criteria.add(Expression.eq("name", GameConfigureConstant.SERVER_PARAM_NAME));
-	    	GlobalConfig globalConfig = (GlobalConfig) criteria.uniqueResult();
-			// 更新记录
-			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-			properties.storeToXML(outStream, null);
-			globalConfig.setValue(outStream.toString("utf-8"));
-			globalConfig.setUpdateTime(new Date());
-			HibernateSessionFactory.getSession().save(globalConfig);
-			info.setResult(F3SWebServiceResult.SUCCESS);
-        } catch (Exception e) {
-        	e.printStackTrace();
-        	info.setResult(F3SWebServiceResult.FAIL);
-        }
-        return toXML(info, getAliasTypes());
+		String name = request.getParameter("VARIABLE_NAME");
+        String value = request.getParameter("VARIABLE_VALUE");
+		GlobalConfig globalconfig = new GlobalConfigDAO().findById(request.getParameter("GLOBALCONFIG_ID"));
+		globalconfig.setName(name);
+		globalconfig.setValue(value);
+        globalconfig.setUpdateTime(new Date());
+        HibernateSessionFactory.getSession().update(globalconfig);
+        EntityInfo<GlobalConfig> info = new EntityInfo<GlobalConfig>();
+        info.setResult(F3SWebServiceResult.SUCCESS);
+        return toXML(info, new Class[] {GlobalConfig.class});
     }
 	
 	/**
@@ -161,56 +107,10 @@ public class ServerConfigureService extends F3SWebService<List<Map>> {
 	 * @return
 	 */
 	public String DELETE_SERVER_CONFIGURE(HttpServletRequest request, HttpServletResponse response) {
-		EntityInfo<List<Map>> info = new EntityInfo<List<Map>>();
-    	try {
-	        String variableName = request.getParameter("VARIABLE_NAME");
-	        String variableNameTager = toStandVariableName(variableName);
-	        Properties properties = ModelUtil.readProperties(GameConfigureConstant.SERVER_PARAM_NAME);
-	        // 重构参数列表[将删除的参数键值不重新加载]
-	        Properties propertiesDest = new Properties();
-	    	for (Entry<Object, Object> param : properties.entrySet()) {
-	    		// 更新变量值
-	    		if (param.getKey().equals(variableNameTager)) {
-	    			continue;
-	    		}
-	    		propertiesDest.put(param.getKey(), param.getValue());
-		    }
-	        
-	        // 数据更新至DB
-	    	// 获取原始记录
-	    	Criteria criteria = HibernateSessionFactory.getSession().createCriteria(GlobalConfig.class);
-	    	criteria.add(Expression.eq("name", GameConfigureConstant.SERVER_PARAM_NAME));
-	    	GlobalConfig globalConfig = (GlobalConfig) criteria.uniqueResult();
-			// 更新记录
-			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-			propertiesDest.storeToXML(outStream, null);
-			globalConfig.setValue(outStream.toString("utf-8"));
-			globalConfig.setUpdateTime(new Date());
-			HibernateSessionFactory.getSession().save(globalConfig);
-			info.setResult(F3SWebServiceResult.SUCCESS);
-        } catch (Exception e) {
-        	e.printStackTrace();
-        	info.setResult(F3SWebServiceResult.FAIL);
-        }
-        return toXML(info, getAliasTypes());
+		GlobalConfig globalconfig = new GlobalConfigDAO().findById(request.getParameter("GLOBALCONFIG_ID"));
+        new GlobalConfigDAO().delete(globalconfig);
+        EntityInfo<GlobalConfig> info = new EntityInfo<GlobalConfig>();
+        info.setResult(F3SWebServiceResult.SUCCESS);
+        return toXML(info, new Class[] {GlobalConfig.class});
     }
-	
-	/**
-	 * 转成标准的变量名
-	 * 
-	 * minVirtualOnlineNumber -> MIN_VIRTUAL_ONLINE_NUMBER
-	 * @param variableName
-	 * @return
-	 */
-	private String toStandVariableName(String variableName) {
-		StringBuffer strBuf = new StringBuffer();
-        char chr[] = variableName.toCharArray();
-        for (int i = 0; i < chr.length; i++) {
-        	if (("" + chr[i]).matches("[A-Z]")) {
-        		strBuf.append("_");
-	        }
-        	strBuf.append("" + chr[i]);
-		}
-        return strBuf.toString().toUpperCase();
-	}
 }
