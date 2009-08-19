fefb39d44b871a751b99978aac1948c9b33db9c3
rcom10002 <rcom10002@e0633416-5d6b-11de-8d7d-01d876f1962d>
2009-08-19 08:09:43 +0000

    csv export

--- f3server/javaweb/info/knightrcom/web/service/ReportBusinessService.java
+++ f3server/javaweb/info/knightrcom/web/service/ReportBusinessService.java
@@ -1,14 +1,27 @@
 package info.knightrcom.web.service;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import info.knightrcom.data.HibernateSessionFactory;
+import info.knightrcom.data.metadata.PeriodlySum;
 import info.knightrcom.data.metadata.PlayerScore;
 import info.knightrcom.util.StringHelper;
+import info.knightrcom.web.model.EntityInfo;
 import info.knightrcom.web.model.entity.ReportBusinessInfo;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.Query;
 import org.hibernate.transform.ResultTransformer;
 import org.hibernate.transform.Transformers;
+import org.supercsv.io.CsvMapWriter;
+import org.supercsv.io.ICsvMapWriter;
+import org.supercsv.prefs.CsvPreference;
 
 public class ReportBusinessService extends F3SWebService<PlayerScore> {
 
@@ -41,5 +54,50 @@ public class ReportBusinessService extends F3SWebService<PlayerScore> {
     @Override
     public Class<?>[] getAliasTypes() {
         return new Class<?>[] {PlayerScore.class, ReportBusinessInfo.class};
+    }
+    
+    /**
+     * 报表导出
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException 
+     */
+	@SuppressWarnings("unchecked")
+	public String CSV_EXPORT(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		EntityInfo<PlayerScore> info = new EntityInfo<PlayerScore>();
+		
+    	Query query = HibernateSessionFactory.getSession().getNamedQuery(getNamedQuery());
+    	processQuerySetting(query, request);
+    	query.setResultTransformer(getResultTransformer());
+    	List<ReportBusinessInfo> list = (List<ReportBusinessInfo>)query.list();
+    	String url = request.getSession().getServletContext().getRealPath("/");
+    	String filename = "BUSINESS_SUM_" + new java.util.Date().getTime() + ".csv";
+    	ICsvMapWriter writer = new CsvMapWriter(new FileWriter(url + filename),
+				CsvPreference.EXCEL_PREFERENCE);
+    	try {
+			final String[] header = new String[] { "玩家ID", "游戏ID", "游戏类别", "游戏时间", "状态", "得分", "时间开始", "结束开始"};
+			// the actual writing
+			writer.writeHeader(header);
+			for (ReportBusinessInfo business : list) {
+				// set up some data to write
+				final HashMap<String, ? super Object> data = new HashMap<String, Object>();
+				data.put(header[0], business.getUserId());
+				data.put(header[1], business.getGameId());
+				data.put(header[2], business.getGameType());
+				data.put(header[3], business.getCreateTime());
+				data.put(header[4], business.getWinandlose());
+				data.put(header[5], business.getScore());
+				data.put(header[6], business.getStartTime());
+				data.put(header[7], business.getEndTime());
+				writer.write(data, header);
+			}
+			info.setTag(filename);
+			info.setResult(F3SWebServiceResult.SUCCESS);
+		} finally {
+			writer.close();
+		}
+		
+        return toXML(info, new Class[] {PeriodlySum.class});
     }
 }
