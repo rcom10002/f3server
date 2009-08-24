5b46c2be1169d1fa34897fbc31ff6af2db885c84
rcom10002 <rcom10002@e0633416-5d6b-11de-8d7d-01d876f1962d>
2009-08-24 06:17:21 +0000

    add groupuser role

--- trunk/f3server/javaweb/info/knightrcom/web/service/ReportScoreService.java
+++ trunk/f3server/javaweb/info/knightrcom/web/service/ReportScoreService.java
@@ -2,6 +2,7 @@ package info.knightrcom.web.service;
 
 import info.knightrcom.data.HibernateSessionFactory;
 import info.knightrcom.data.metadata.PeriodlySum;
+import info.knightrcom.data.metadata.PlayerProfile;
 import info.knightrcom.util.StringHelper;
 import info.knightrcom.web.model.EntityInfo;
 import info.knightrcom.web.model.entity.ReportScoreInfo;
@@ -34,8 +35,13 @@ public class ReportScoreService extends F3SWebService<PeriodlySum> {
         query.setTimestamp(1, StringHelper.toTimeStamp(request.getParameter("TO_DATE"), "yyyyMMdd"));
         String userId = StringHelper.escapeSQL(request.getParameter("USER_ID")) == null ? "" : StringHelper.escapeSQL(request.getParameter("USER_ID"));
         query.setString(2, "%" + userId + "%");
-        query.setString(3, StringHelper.escapeSQL(request.getParameter("USER_ID")));
-        query.setString(4, userId + "%");
+        PlayerProfile profile = (PlayerProfile) request.getSession().getAttribute("PROFILE");
+        if ("GroupUser".equals(profile.getRole())) {
+        	query.setString(3, profile.getUserId());
+        } else {
+        	query.setString(3, null);
+        }
+        query.setString(4, profile.getUserId() + "%");
 	}
 	
 	/**
@@ -59,8 +65,13 @@ public class ReportScoreService extends F3SWebService<PeriodlySum> {
 		
         String userId = StringHelper.escapeSQL(request.getParameter("USER_ID")) == null ? "" : StringHelper.escapeSQL(request.getParameter("USER_ID"));
         query.setString(12, "%" + userId + "%");
-        query.setString(13, StringHelper.escapeSQL(request.getParameter("USER_ID")));
-        query.setString(14, userId + "%");
+        PlayerProfile profile = (PlayerProfile) request.getSession().getAttribute("PROFILE");
+        if ("GroupUser".equals(profile.getRole())) {
+        	query.setString(13, profile.getUserId());
+        } else {
+        	query.setString(13, null);
+        }
+        query.setString(14, profile.getUserId() + "%");
 	}
 	
     @Override
