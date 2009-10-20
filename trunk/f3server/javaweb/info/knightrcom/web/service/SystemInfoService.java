73fb22979387393d0c5fb751c2d275d4d44ddfeb
rcom10002 <rcom10002@e0633416-5d6b-11de-8d7d-01d876f1962d>
2009-10-20 15:24:28 +0000

    * ゲームビデオの放送について      *      * このユーザはゲームを参加しない時、いろいろな情報を取得しない。      * ゲームを参加しないユーザはビデオの放送を使用できる。      * 正確の検索条件はユーザのプロファイルIDとゲームIDとPLAYVEDIO状態を使用する。      * 検索件数は一件以上の時、ログを記録だけ。      * ゼロ件数を検索する時、ユーザプロファイルテーブルのレコードの分数フィールドを更新して、      * 分数テーブルのに新しいレコードを追加する、ログを記録する。      *      * 業務をよく考えして、テストをして下さいな！

--- trunk/f3server/javaweb/info/knightrcom/web/service/SystemInfoService.java
+++ trunk/f3server/javaweb/info/knightrcom/web/service/SystemInfoService.java
@@ -13,6 +13,8 @@ import info.knightrcom.util.SystemLogger;
 import info.knightrcom.web.model.EntityInfo;
 
 import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -56,25 +58,39 @@ public class SystemInfoService extends F3SWebServiceAdaptor<GameRecord> {
      * @param response
      * @return
      */
-    public String LOAD_GAME_RECORD(HttpServletRequest request, HttpServletResponse response) {
+    @SuppressWarnings("unchecked")
+	public String LOAD_GAME_RECORD(HttpServletRequest request, HttpServletResponse response) throws Exception {
     	GameRecord gameRecord = new GameRecordDAO().findById(request.getParameter("GAME_ID"));
     	PlayerProfile playerProfile = new PlayerProfileDAO().findById(request.getParameter("CURRENT_PROFILE_ID"));
     	// 更新看过录像标识
-		PlayerScore playerScore = (PlayerScore)HibernateSessionFactory.getSession().createCriteria(
+		List<PlayerScore> playerScores = HibernateSessionFactory.getSession().createCriteria(
 				PlayerScore.class).add(
                         Restrictions.eq("profileId", request.getParameter("CURRENT_PROFILE_ID"))).add(
-                        Restrictions.eq("gameId", request.getParameter("GAME_ID"))).uniqueResult();
+                        Restrictions.eq("gameId", request.getParameter("GAME_ID"))).add(
+                        Restrictions.eq("status", "PLAYVEDIO")).list();
     	// 查看录像是否扣底分
-    	if (!"PLAYVEDIO".equals(playerScore.getStatus())) {
+    	if (playerScores == null || playerScores.size() == 0) {
+    		// 在用户信息中保存总积分
     		playerProfile.setCurrentScore(playerProfile.getCurrentScore() - 2 * gameRecord.getScore());
     		HibernateSessionFactory.getSession().save(playerProfile);
+    		// 将扣分记录写入用户积分中
+            PlayerScore playerScore = new PlayerScore();
+            playerScore.setScoreId(UUID.randomUUID().toString());
+            playerScore.setProfileId(playerProfile.getProfileId());
+            playerScore.setGameId(gameRecord.getGameId());
+            playerScore.setUserId(playerProfile.getUserId());
+            playerScore.setCurrentNumber("-1");
+            playerScore.setCurScore(0d); // 玩家当前得分
+            playerScore.setSysScore(2 * gameRecord.getScore()); // 系统当前得分
+            playerScore.setOrgScores(playerProfile.getCurrentScore() + 2 * gameRecord.getScore()); // 玩家原始总积分
+            playerScore.setCurScores(playerProfile.getCurrentScore()); // 玩家当前总积分
     		playerScore.setStatus("PLAYVEDIO");
     		HibernateSessionFactory.getSession().save(playerScore);
     	}
         EntityInfo<GameRecord> info = new EntityInfo<GameRecord>();
         info.setEntity(gameRecord);
         // 看录像需要写日志
-        LogInfo logInfo = SystemLogger.createLog("SystemInfoService Successfully", null, "user [" + playerProfile.getUserId() +"] at system time [" + StringHelper.formatTimeStamp(new Date()) + "] view vedio game id is [" + gameRecord.getGameId() + "]", LogType.SYSTEM_LOG);
+        LogInfo logInfo = SystemLogger.createLog("SystemInfoService Successfully", null, "user [" + playerProfile.getUserId() +"] at system time [" + StringHelper.formatTimeStamp(new Date()) + "] view vedio, game id is [" + gameRecord.getGameId() + "]", LogType.SYSTEM_LOG);
         HibernateSessionFactory.getSession().save(logInfo);
         return toXML(info, getAliasTypes());
     }
@@ -103,13 +119,25 @@ public class SystemInfoService extends F3SWebServiceAdaptor<GameRecord> {
      * @param request
      * @param response
      * @return
+     * @deprecated
+     * 
+     * ゲームビデオの放送について
+     * 
+     * このユーザはゲームを参加しない時、いろいろな情報を取得しない。
+     * ゲームを参加しないユーザはビデオの放送を使用できる。
+     * 正確の検索条件はユーザのプロファイルIDとゲームIDとPLAYVEDIO状態を使用する。
+     * 検索件数は一件以上の時、ログを記録だけ。
+     * ゼロ件数を検索する時、ユーザプロファイルテーブルのレコードの分数フィールドを更新して、
+     * 分数テーブルのに新しいレコードを追加する、ログを記録する。
+     * 
+     * 業務をよく考えして、テストをして下さいな！
      */
     public String READ_PLAYER_INFO(HttpServletRequest request, HttpServletResponse response) {
     	EntityInfo<GameRecord> info = new EntityInfo<GameRecord>();
     	Query query = HibernateSessionFactory.getSession().getNamedQuery(getNamedQuery());
     	processQuerySetting(query, request);
     	query.setResultTransformer(getResultTransformer());
-    	info.setTag(query.uniqueResult());
+    	info.setTag(query.list().get(0));
         return toXML(info, getAliasTypes());
     }
     
