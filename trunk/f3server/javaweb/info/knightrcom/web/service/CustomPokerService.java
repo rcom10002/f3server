ef3cee65feac448b6b2761d1f367d0d37f6cee32
rcom10002 <rcom10002@e0633416-5d6b-11de-8d7d-01d876f1962d>
2009-11-16 09:04:08 +0000

    自主牌型设置

--- trunk/f3server/javaweb/info/knightrcom/web/service/CustomPokerService.java
+++ trunk/f3server/javaweb/info/knightrcom/web/service/CustomPokerService.java
@@ -1,5 +1,7 @@
 package info.knightrcom.web.service;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,39 +14,35 @@ import info.knightrcom.web.model.EntityInfo;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.Criteria;
 import org.hibernate.Query;
+import org.hibernate.criterion.Expression;
 import org.hibernate.transform.ResultTransformer;
 
 public class CustomPokerService extends F3SWebService<GlobalConfig> {
 
 	@Override
 	public Class<?>[] getAliasTypes() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Class<?>[] {GlobalConfig.class};
 	}
 
 	@Override
 	public String getNamedQuery() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getNamedQueryForCount() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ResultTransformer getResultTransformer() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void processQuerySetting(Query query, HttpServletRequest request) {
-		// TODO Auto-generated method stub
-		
 	}
 	
 	/**
@@ -54,22 +52,32 @@ public class CustomPokerService extends F3SWebService<GlobalConfig> {
      * @throws Exception
      */
     public String READ_POKER(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    	String pokersValue = "";
-    	List<GlobalConfig> list = new GlobalConfigDAO().findByType("CUSTOM_POKER");
-    	if (list != null && list.size() > 0) {
-    		pokersValue = list.get(0).getValue() + "~" + list.get(0).getStatus();
-    	} else {
-	    	Red5Poker[][] eachShuffledPokers = Red5Poker.shuffle();
+    	EntityInfo<GlobalConfig> info = createEntityInfo(null, F3SWebServiceResult.SUCCESS);
+    	String method = request.getParameter("METHOD");
+    	if (method != null && method.length() > 0) {
+    		Red5Poker[][] eachShuffledPokers = Red5Poker.shuffle();
 	        StringBuilder builder = new StringBuilder();
 	    	for (int m = 0; m < eachShuffledPokers.length; m++) {
 	            for (int n = 0; n < eachShuffledPokers[m].length; n++) {
 	                builder.append(eachShuffledPokers[m][n].getValue() + ",");
 	            }
 	    	}
-	    	pokersValue = builder.toString().replaceAll(",$", "");
+	    	info.setTag(builder.toString().replaceAll(",$", ""));
+    	} else {
+    		List<GlobalConfig> list = new GlobalConfigDAO().findByType("CUSTOM_POKER");
+    		// 排序
+    		Collections.sort(list, new Comparator<GlobalConfig>() {
+				public int compare(GlobalConfig o1, GlobalConfig o2) {
+					if (o1.getCreateTime().getTime() > o2.getCreateTime().getTime()) {
+						return -1;
+					} else if (o1.getCreateTime().getTime() < o2.getCreateTime().getTime()) {
+						return 1;
+					}
+					return 0;
+				}
+    		});
+    		info.setTag(list.toArray());
     	}
-    	EntityInfo<GlobalConfig> info = createEntityInfo(null, F3SWebServiceResult.SUCCESS);
-    	info.setTag(pokersValue);
     	return toXML(info, GlobalConfig.class);
 	}
     
@@ -80,15 +88,30 @@ public class CustomPokerService extends F3SWebService<GlobalConfig> {
      * @return
      * @throws Exception
      */
-    public String SAVE_CUSTOM_POKER(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @SuppressWarnings("unchecked")
+	public String SAVE_CUSTOM_POKER(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	String id = request.getParameter("ID");
+    	String name = request.getParameter("NAME");
     	String strUpCards = request.getParameter("UP_POKERS");
     	String strDownCards = request.getParameter("DOWN_POKERS");
     	String isOpen = request.getParameter("IS_OPEN");
     	EntityInfo<GlobalConfig> info = createEntityInfo(null, F3SWebServiceResult.SUCCESS);
     	try {
-	    	GlobalConfig config = new GlobalConfig();
-	    	config.setGlobalConfigId(UUID.randomUUID().toString());
-	    	config.setName("CUSTOM_POKER");
+    		GlobalConfig config = new GlobalConfigDAO().findById(id);
+    		if (config == null) {
+    			// 新增时判断自主牌型名称是否重复
+    			Criteria criteria = HibernateSessionFactory.getSession().createCriteria(GlobalConfig.class);
+    			criteria.add(Expression.eq("name", name));
+    			criteria.add(Expression.eq("type", "CUSTOM_POKER"));
+    			List<GlobalConfig> result = criteria.list();
+    			if (result != null && result.size() > 0) {
+    				info.setResult(F3SWebServiceResult.WARNING);
+    				return toXML(info, getAliasTypes());
+    			}
+		    	config = new GlobalConfig();
+	    		config.setGlobalConfigId(UUID.randomUUID().toString());
+    		} 
+	    	config.setName(name);
 	    	config.setValue(strUpCards + "~" + strDownCards);
 	    	config.setStatus(isOpen);
 	    	config.setType("CUSTOM_POKER");
